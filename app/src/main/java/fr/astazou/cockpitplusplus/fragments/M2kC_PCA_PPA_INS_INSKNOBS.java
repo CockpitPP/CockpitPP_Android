f3fe3a213c14d21008dce3d50a76c8092dae9aaa
astazou <simon.cvr@gmail.com>
2017-12-15 00:04:18 +0100

    fix rotation bug of fragment resizing in fragment

--- app/src/main/java/fr/astazou/cockpitplusplus/fragments/M2kC_PCA_PPA_INS_INSKNOBS.java
+++ app/src/main/java/fr/astazou/cockpitplusplus/fragments/M2kC_PCA_PPA_INS_INSKNOBS.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import fr.astazou.cockpitplusplus.R;
 
@@ -27,21 +28,9 @@ public class M2kC_PCA_PPA_INS_INSKNOBS extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_m2kc_pca_ppa_ins_insknob, container, false);
-
-        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
-        Fragment ppa = new M2kC_PPA();
-        transaction.replace(R.id.ppa, ppa).commit();
-        transaction = getChildFragmentManager().beginTransaction();
-        Fragment pca = new M2kC_PCA();
-        transaction.replace(R.id.pca, pca).commit();
-        transaction = getChildFragmentManager().beginTransaction();
-        Fragment ins = new M2kC_INS();
-        transaction.replace(R.id.ins, ins).commit();
-        transaction = getChildFragmentManager().beginTransaction();
-        Fragment insknobs = new M2kC_INS_Knob();
-        transaction.replace(R.id.insknobs, insknobs).commit();
-        return view;
+        FrameLayout frameLayout = new FrameLayout(getActivity());
+        populateViewForOrientation(inflater, frameLayout);
+        return frameLayout;
     }
 
     @Override
@@ -60,6 +49,25 @@ public class M2kC_PCA_PPA_INS_INSKNOBS extends Fragment {
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         // Reload current fragment
-        getFragmentManager().beginTransaction().detach(this).attach(this).commit();
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        populateViewForOrientation(inflater, (ViewGroup) getView());
+    }
+
+    private void populateViewForOrientation(LayoutInflater inflater, ViewGroup viewGroup) {
+        viewGroup.removeAllViewsInLayout();
+        View subview = inflater.inflate(R.layout.fragment_m2kc_pca_ppa_ins_insknob, viewGroup);
+
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        Fragment ppa = new M2kC_PPA();
+        transaction.replace(R.id.ppa, ppa).commit();
+        transaction = getChildFragmentManager().beginTransaction();
+        Fragment pca = new M2kC_PCA();
+        transaction.replace(R.id.pca, pca).commit();
+        transaction = getChildFragmentManager().beginTransaction();
+        Fragment ins = new M2kC_INS();
+        transaction.replace(R.id.ins, ins).commit();
+        transaction = getChildFragmentManager().beginTransaction();
+        Fragment insknobs = new M2kC_INS_Knob();
+        transaction.replace(R.id.insknobs, insknobs).commit();
     }
 }
